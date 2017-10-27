fc5e0affcc52121670b76834b6b7ed02f9685327
Sarah <sarah.hyun93@gmail.com>
2017-10-26 23:43:37 -0700

    added carousel

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+var x = 0;
+slideShow();
+
+function slideShow() {
+    var image = document.getElementsByClassName("main-img");
+    for (var i = 0; i < image.length; i++) {
+       image[i].style.display = "none";  
+    }
+
+    if (x >= image.length) {x = 0}    
+    image[x].style.display = "inline-block";  
+    x ++;
+    setTimeout(slideShow, 2500); 
+}
