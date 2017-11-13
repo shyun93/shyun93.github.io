69c4795e55b2ab8a7cffb0b02a795da497f8eaea
Sarah Hyun <31718553+shyun93@users.noreply.github.com>
2017-11-13 15:46:21 -0800

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -5,12 +5,10 @@ function slideShow() {
     var image = document.getElementsByClassName("main-img");
     for (var i = 0; i < image.length; i++) {
        image[i].style.display = "none"; 
-       image[i].style.transition = "all 0.3s"
     }
 
     if (x >= image.length) {x = 0}    
     image[x].style.display = "inline-block";
-    image[i].style.transition = "all 0.3s"
     x ++;
     setTimeout(slideShow, 2500); 
 }
