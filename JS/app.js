4142f5391e0c3f588c2aaa75988c2458224152f3
Seung Jae Hyun <seungjaehyun@Seungs-MacBook.local>
2017-12-17 10:10:38 -0800

    added shop file

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,28 @@
-var x = 0;
-slideShow();
+// var x = 0;
+// slideShow();
 
-function slideShow() {
-    var image = document.getElementsByClassName("main-img");
-    for (var i = 0; i < image.length; i++) {
-       image[i].style.display = "none"; 
-    }
+// function slideShow() {
+//     var image = document.getElementsByClassName("main-img");
+//     for (var i = 0; i < image.length; i++) {
+//        image[i].style.display = "none"; 
+//     }
 
-    if (x >= image.length) {x = 0}    
-    image[x].style.display = "inline-block";
-    x ++;
-    setTimeout(slideShow, 2500); 
-}
+//     if (x >= image.length) {x = 0}    
+//     image[x].style.display = "inline-block";
+//     x ++;
+//     setTimeout(slideShow, 2500); 
+// } 
+// this code was with javascript-- could not figure out how to fade in and out
+
+
+//this bottom code is with jQuery with fade in and outs
+$("#slideshow > div:gt()").css("display", "none");
+
+setInterval(function() {
+ $('#slideshow > div:first')
+   .fadeOut(1000)
+   .next()
+   .fadeIn(1000)
+   .end()
+   .appendTo('#slideshow');
+}, 3000);
